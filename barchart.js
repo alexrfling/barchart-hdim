cc8a3ca3a7f3852878910c691cdd99aca90e44c0
flinga <flinga@cs.washington.edu>
2016-12-01 21:46:23 -0800

    update h/w

--- barchart.js
+++ barchart.js
@@ -3,19 +3,7 @@ function barchart(id, height, data) {
                                   { top: 5, bottom: 5, left: 5, right: 5 }, height);
   var byName = true;
   var descending = true;
-  var filter = container.SVG.append("filter")
-                  .attr("id", "shadow")
-                  .attr("x", -2)
-                  .attr("y", -2)
-                  .attr("width", 10)
-                  .attr("height", 10);
-  filter.append("feGaussianBlur")
-        .attr("in", "SourceAlpha")
-        .attr("stdDeviation", 4);
-  filter.append("feOffset").attr("dx", 0).attr("dy", 0);
-  var filterMerge = filter.append("feMerge");
-  filterMerge.append("feMergeNode");
-  filterMerge.append("feMergeNode").attr("in", "SourceGraphic");
+  addDropShadowFilter(container.SVG, "shadow");
 
   container.resize();
 
