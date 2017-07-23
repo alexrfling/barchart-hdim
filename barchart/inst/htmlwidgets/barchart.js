9c16721ef82a54a3bf7e2a071d3099806da99737
flinga <flinga@cs.washington.edu>
2017-07-22 17:54:23 -0700

    submodule update, adjust YAML and JS, rebuild

--- barchart/inst/htmlwidgets/barchart.js
+++ barchart/inst/htmlwidgets/barchart.js
@@ -8,7 +8,7 @@ HTMLWidgets.widget({
 
     factory: function (el, width, height) {
 
-        var chart = new Barchart(el.id);
+        var chart = new d3.Barchart(el.id);
 
         // booleans converted from R may be true, false, 'TRUE', or 'FALSE'
         function getBool (bool) {
